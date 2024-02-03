4119f5a155da9356a565d94c1140d2e97d87dacc
ilia <jjnn95@yandex.ru>
2024-02-03 10:35:46 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
     server: {
         hmr: {
-            host: "localhost",
+            host: "8081",
         },
         watch: {
             usePolling: true,
