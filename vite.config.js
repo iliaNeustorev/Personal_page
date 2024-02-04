66993e0a15d0752fee433f424eda02a9370bb70c
ilia <jjnn95@yandex.ru>
2024-02-04 13:51:56 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,6 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    server: {
-      https: false,
-      cors: false,
-      host: 'localhost',
-      hmr: {
-        https: false,
-        cors: false,
-        host: 'localhost',
-      },
-    },
     plugins: [
         laravel({
             input: ["resources/css/app.scss", "resources/js/main.js"],
