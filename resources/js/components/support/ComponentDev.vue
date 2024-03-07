1645eda605238a5699350864ef4ff9edab659313
ilia <jjnn95@yandex.ru>
2024-03-07 11:04:01 +0300

    add test route fix

---
+++ resources/js/components/support/ComponentDev.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <p class="title is-3 has-text-primary-dark mx-2">
+            Тестовая среда для разработки
+        </p>
+    </div>
+    
+</template>
+    
+<script>
+export default {
+    async created() {
+        await this.$api.home.testRoute();
+    }
+}
+</script>
