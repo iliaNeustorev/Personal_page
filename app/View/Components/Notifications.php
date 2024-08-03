56bed47fb3ba4188b538968da68e15cfb14627dd
ilia <jjnn95@yandex.ru>
2024-08-03 08:16:30 +0300

    fix notify

--- app/View/Components/Notifications.php
+++ app/View/Components/Notifications.php
@@ -7,27 +7,17 @@ use Illuminate\Contracts\View\View;
 
 class Notifications extends Component
 {
-    /**
-     *
-     * @param string|null $message
-     * @param boolean $hasMessage
-     * @param array $messages
-     */ 
-    public function __construct(
-        public string|null $message,
-        public bool $hasMessage,
-        public array $messages,
-    )
+    public string|null $message;
+    public bool $hasMessage;
+    public array $messages;
+    
+    public function __construct()
     {
         $this->message = session()->get('notification');
         $this->hasMessage = $this->message !== null;
         $this->messages = $this->hasMessage ? config('app-notifications') : [];
     }
 
-    /**
-     *
-     * @return View
-     */
     public function render() : View
     {
         return view('components.notifications');
