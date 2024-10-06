8a23d28785556b0c6d5b66cf3df3837c857a64fa
ilia <jjnn95@yandex.ru>
2024-10-06 08:35:21 +0300

    fix config

--- app/Http/Controllers/Admin.php
+++ app/Http/Controllers/Admin.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Mail\NewFeedback;
-use App\Models\Feedback;
 use App\Models\User;
-use App\Services\UserService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Mail;
 
 class Admin extends Controller
 {
@@ -21,9 +17,10 @@ class Admin extends Controller
      */
     public function test(Request $request): JsonResponse
     {
-        $feedback = Feedback::find(1);
-        $moder = User::where('email', UserService::MODER_EMAIL_SEND)->firstOrfail();
-        Mail::to($moder)->send(new NewFeedback($feedback));
+        $test = '1111';
+        dispatch(function () use ($test) {
+            Log::info('Джоб выполнен' .  $test);
+        })->onQueue('test');
         return response()->json([], 200);
     }
 
