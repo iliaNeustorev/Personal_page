1af0d386991c2fc6ab8a54508c249c04a651dd1e
ilia <jjnn95@yandex.ru>
2024-12-07 14:03:06 +0300

    fix

--- resources/js/components/nav/Footer.vue
+++ resources/js/components/nav/Footer.vue
@@ -2,9 +2,8 @@
     <footer class="footer">
         <div class="content has-text-centered">
             <p>
-                <strong>Разработчик </strong>
-                <a href="https://vk.com/id42184640">Неустроев Илья </a>
-                <i>Сайт разработан на <a href="https://laravel.com/">Laravel 10</a>|<a href="https://vuejs.org/">Vue 3</a>|<a href="https://bulma.io/">Bulma</a> в 2024 году </i>
+                <i>Сайт разработан на <a href="https://laravel.com/">Laravel 10</a>|<a href="https://vuejs.org/">Vue
+                        3</a>|<a href="https://bulma.io/">Bulma</a> в 2024 году </i>
             </p>
         </div>
     </footer>
